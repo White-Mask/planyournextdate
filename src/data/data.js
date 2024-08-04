7d13fc0a550857fc642de6562ac4965141f3ab27
Jorge Ahumada <grandekoke@gmail.com>
2024-08-04 03:17:22 -0400

    refactor: add more data, update ux, update ui

--- src/data/data.js
+++ src/data/data.js
@@ -3,69 +3,113 @@ export const data = [
     {
         "name": "Picnic",
         "image": "https://www.finedininglovers.com/es/sites/g/files/xknfdk1706/files/styles/im_landscape_100/public/2024-04/pic-nic.jpg.webp?itok=jcLOYyHW",
-        "price": "Sin especificar",
-        "location": "Sin especificar",
-        "category": "desconectar"
+        "category": "desconectar",
+        "description": "Un picnic es perfecto para una cita porque ofrece un ambiente relajado y natural donde pueden disfrutar de la compañía mutua al aire libre. Compartir una comida en un parque o junto a un lago crea una atmósfera íntima y tranquila, ideal para charlar y conocer mejor a la otra persona.",
+    },
+    {
+        "name": "Senderismo",
+        "image": "https://campinglafundicion.es/wp-content/uploads/2018/09/senderismo-1066x533.jpg",
+        "category": "desconectar",
+        "description": "El senderismo es una actividad saludable y relajante que les permite disfrutar de la naturaleza y desconectar del estrés diario. Pueden explorar rutas cercanas a la ciudad o adentrarse en la montaña para disfrutar de hermosos paisajes y compartir momentos especiales juntos.",
+    },
+    {
+        "name": "Visita a un museo",
+        "image": "https://www.lavanguardia.com/files/article_main_microformat/uploads/2021/04/22/60817fe970aa9.jpeg",
+        "category": "desconectar",
+        "description": "Visitar un museo es una actividad cultural y enriquecedora que les permite conocer más sobre el arte, la historia y la ciencia juntos. Pueden explorar las exposiciones, aprender cosas nuevas y compartir sus impresiones y opiniones sobre las obras y objetos expuestos.",
+    },
+    {
+        "name": "Cine",
+        "image": "https://arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/UKVWSGPUORCE3OJXI7BQYN5YLY.jpg",
+        "category": "desconectar",
+        "description": "Ir al cine es una opción clásica para una cita porque les permite disfrutar de una película juntos en un ambiente relajado y cómodo. Pueden elegir una película que les guste a ambos, compartir palomitas y refrescos, y disfrutar de una velada entretenida y divertida.",
+    },
+    {
+        "name": "Cafetería",
+        "image": "https://lacafeteriatalca.cl/wp-content/uploads/2020/08/carrusel1.jpg",
+        "category": "cafeteria",
+        "description": "Tomar un café juntos en una cafetería es una opción sencilla y relajada para una cita. Pueden disfrutar de una conversación tranquila y compartir momentos especiales mientras degustan una deliciosa bebida caliente o fría.",
+    },
+    {
+        "name": "Heladería",
+        "image": "https://mejisa.com/wp-content/uploads/2021/11/heladeria.jpg",
+        "category": "cafeteria",
+        "description": "Ir a una heladería es una opción refrescante y dulce para una cita. Pueden disfrutar de un helado juntos en un ambiente relajado y cómodo, y compartir risas y buenos momentos mientras degustan sabores deliciosos y variados.",
+    },
+    //Conciertos
+    {
+        "name": "Concierto en vivo",
+        "image": "https://fotografias.larazon.es/clipping/cmsimages01/2023/11/17/B509E9E7-1C8A-4542-8074-5554F9C9D421/conciertos_98.jpg?crop=1120,630,x41,y0&width=1900&height=1069&optimize=low&format=webply",
+        "category": "conciertos",
+        "description": "Ir a un concierto en vivo es una opción emocionante y divertida para una cita. Pueden disfrutar de la música en directo, bailar y cantar juntos, y compartir la emoción y la energía del espectáculo en un ambiente festivo y animado.",
     },
     //Nocturnas
     {
         "name": "Restaurante",
         "image": "https://img.freepik.com/fotos-premium/pareja-enamorada-restaurante-luces-al-fondo_662214-13292.jpg",
-        "price": "Sin especificar",
-        "location": "Varias opciones en Santiago",
-        "category": "nocturnas"
+        "category": "nocturnas",
+        "description": "Una cena en un restaurante es una opción clásica para una cita. Pueden elegir un lugar romántico y acogedor, o uno más animado y divertido, según sus gustos. La comida y la bebida siempre son una buena excusa para disfrutar de una velada juntos.",
+    },
+    {
+        "name": "Bar",
+        "image": "https://i0.wp.com/60mais.com.br/wp-content/uploads/2019/01/bares-e-programas-noturnos-para-60.jpg?fit=1000%2C664&ssl=1",
+        "category": "nocturnas",
+        "description": "Ir a un bar es una opción relajada y divertida para una cita nocturna. Pueden disfrutar de una copa juntos en un ambiente animado y acogedor, y compartir risas y buenos momentos mientras degustan sus bebidas favoritas.",
+    },
+    {
+        "name": "Discoteca",
+        "image": "https://madriddiferente.com/wp-content/uploads/2018/05/graf-destacada-770x466.jpg",
+        "category": "nocturnas",
+        "description": "Ir a una discoteca es una opción divertida y animada para una cita nocturna. Pueden bailar juntos, disfrutar de la música y la atmósfera festiva, y compartir la emoción y la energía de la pista de baile en un ambiente vibrante y lleno de vida.",
+    },
+    {
+        "name": "Jazz club",
+        "image": "https://lefifa.com/uploads/images/jazz-club-owner/_large/Jazz_Club_Owner_-_0174-_Sharpened_-2_-_FIFA.jpg",
+        "category": "nocturnas",
+        "description": "Ir a un club de jazz es una opción elegante y sofisticada para una cita nocturna. Pueden disfrutar de la música en vivo, la atmósfera íntima y acogedora, y compartir momentos especiales mientras se dejan llevar por los ritmos suaves y envolventes del jazz.",
+    },
+    {
+        "name": "Show de comedia",
+        "image": "https://fotos.orepublicano.com.br/uploads/fotos/1614713989_603e94856becc.jpeg",
+        "category": "nocturnas",
+        "description": "Ir a un show de comedia es una opción divertida y entretenida para una cita nocturna. Pueden reír juntos, disfrutar de las actuaciones cómicas y compartir momentos de diversión y buen humor en un ambiente festivo y animado.",
     },
     // Caseras
     {
-        "name": "Cocinar juntos",
+        "name": "Cocinar",
         "image": "https://www.fmdos.cl/wp-content/uploads/2017/08/GettyImages-98471686-1024x683.jpg",
-        "price": "Sin especificar",
-        "location": "En casa",
-        "category": "caseras"
+        "category": "caseras",
+        "description": "Cocinar juntos es una actividad divertida y creativa que les permite trabajar en equipo y compartir un momento especial. Pueden preparar una receta nueva o cocinar su plato favorito, y luego disfrutarlo juntos en un ambiente relajado y cómodo.",
     },
     {
         "name": "Juegos de mesa",
         "image": "https://www.educaciontrespuntocero.com/wp-content/uploads/2023/07/destacada-juegos-860x574.jpg",
-        "price": "Sin especificar",
-        "location": "En casa",
-        "category": "caseras"
+        "category": "caseras",
+        "description": "Los juegos de mesa son una forma divertida y entretenida de pasar tiempo juntos en casa. Pueden elegir un juego clásico que ya conozcan o probar uno nuevo que les permita desafiar sus habilidades y competir de manera amistosa.",
     },
     {
-        "name": "Ver de películas / series",
+        "name": "Ver películas o series",
         "image": "https://www.eluniverso.com/resizer/v2/AEWN27WM5VG4HENU7XFTHG5LRA.jpeg?auth=93259134244e0df5a02b940cf9cf61504a0e38f2122ec797b2e57a88bfa35942&width=1191&height=670&quality=75&smart=true",
-        "price": "Sin especificar",
-        "location": "En casa",
-        "category": "caseras"
+        "category": "caseras",
+        "description": "Ver películas o series juntos es una forma relajada y cómoda de disfrutar de la compañía mutua. Pueden elegir un género que les guste a ambos o explorar nuevas opciones para descubrir películas y series interesantes que puedan disfrutar juntos.",
     },
     // Competitivas
     {
         "name": "Bowling",
         "image": "https://images2-mega.cdn.mdstrm.com/meganoticias/2023/06/27/_418137_1_649b3e18a74bc.jpg?d=1200x675",
-        "price": {
-            "lower": 18000,
-            "higher": 23000
-        },
-        "location": "Varias opciones en Santiago",
-        "category": "competitivas"
+        "category": "competitivas",
+        "description": "El bowling es una actividad divertida y competitiva que les permite desafiar sus habilidades y competir de manera amistosa. Pueden disfrutar de una tarde o noche de bolos en un ambiente relajado y cómodo, y compartir risas y buenos momentos juntos.",
     },
     {
         "name": "Patinaje en hielo",
         "image": "https://www.cerogrado.cl/wp-content/uploads/2024/07/IMG_7463-scaled.jpg",
-        "price": {
-            "lower": 10000,
-            "higher": 12000
-        },
-        "location": "Varias opciones en Santiago",
-        "category": "competitivas"
+        "category": "competitivas",
+        "description": "El patinaje en hielo es una actividad emocionante y desafiante que les permite disfrutar de la compañía mutua mientras se divierten y se ejercitan juntos. Pueden deslizarse por la pista de hielo, probar nuevos trucos y competir de manera amistosa para ver quién es el mejor patinador.",
     },
     {
         "name": "Karting",
         "image": "https://images2-mega.cdn.mdstrm.com/meganoticias/2023/07/14/419844_2_64b1ac83b0055.jpg",
-        "price": {
-            "lower": 15000,
-            "higher": 20000
-        },
-        "location": "Varias opciones en Santiago",
-        "category": "competitivas"
+        "category": "competitivas",
+        "description": "El karting es una actividad emocionante y competitiva que les permite disfrutar de la velocidad y la adrenalina juntos. Pueden competir en la pista, probar sus habilidades de conducción y ver quién es el más rápido en completar el recorrido.",
     },
 ];
