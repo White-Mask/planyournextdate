a137531f6f914c59e8c3d1c4728967051c8e6e71
Jorge Ahumada <grandekoke@gmail.com>
2024-08-04 19:45:02 -0400

    refactor: rename variable sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import { sitemapPlugin } from '@astrojs/sitemap';
+import { sitemap } from '@astrojs/sitemap';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://planyournextdate.xyz',
-  integrations: [tailwind(), sitemapPlugin()],
+  integrations: [tailwind(), sitemap()],
 });
