656a6bce4f73b19009b850d42ecb79d0024c4b6c
Jorge Ahumada <grandekoke@gmail.com>
2024-08-10 00:18:16 -0400

    refactor: update service worket

--- public/sw.js
+++ public/sw.js
@@ -6,29 +6,28 @@ self.addEventListener('install', function(event) {
         "/index.html",
         "/manifest.json",
         "/favicon.ico",
-        '/favicon.webp',
+        "/favicon.webp",
         "/android-chrome-192x192.png",
         "/android-chrome-512x512.png",
         "/apple-touch-icon.png",
-        '/img/aventura.svg',
-        '/img/cultura.svg',
-        '/img/desconectar.svg',
-        '/img/naturaleza.svg',
-        '/img/casera.svg',
-        '/img/nocturna.svg',
-        '/_astro/montserra....BDA6280a.woff2',
-        '/_astro/hoisted.Bu7JbD2a.js',
-        '/_astro/index.CLwaRr0z.css'
-      ].map(function(url) {
-        return fetch(url).then(function(response) {
-          if (!response.ok) {
-            throw new TypeError('Request failed: ' + url);
-          }
-          return cache.put(url, response);
-        }).catch(function(error) {
-          console.error('Error caching:', url, error);
-        });
-      }));
+        "/img/aventura.svg",
+        "/img/cultura.svg",
+        "/img/desconectar.svg",
+        "/img/naturaleza.svg",
+        "/img/casera.svg",
+        "/img/nocturna.svg",
+        "/_astro/montserrat-latin-wght-normal.BDA6280a.woff2"
+      ]);
+    }).catch(function(error) {
+      console.error('Error caching assets during install:', error);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
